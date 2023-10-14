aa3accfbb6ee5e4362f1dd88cb132bd506743f53
wangtong <realwangtong@163.com>
2023-10-14 09:20:36 +0800

    链表和栈基础题目

---
+++ src/basic/class03/Code02_DeleteGivenValue.java
@@ -0,0 +1,37 @@
+package basic.class03;
+
+/**
+ * @author wangt
+ * @create 2023/10/10 19:20
+ */
+public class Code02_DeleteGivenValue {
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+    // head = removeValue(head, 2);
+    public static Node removeValue(Node head, int num) {
+        while (head != null) {
+            if (head.value != num) {
+                break;
+            }
+            head = head.next;
+        }
+        Node pre = head;
+        Node cur = head;
+        while (cur != null) {
+            if (cur.value == num) {
+                pre.next = cur.next;
+            } else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+        return head;
+    }
+}
